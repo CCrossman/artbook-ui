4290ad1da910bba86a8113a6de41a630086a7b20
Chris Crossman <christopher.j.crossman@gmail.com>
2026-01-25 22:38:51 +0000

    Savepoint 01

--- artbook-ui/src/router.js
+++ artbook-ui/src/router.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from './pages/LoginPage.vue'
 import ViewerPage from './pages/ViewerPage.vue'
 import UploadPage from './pages/UploadPage.vue'
 import GalleryPage from './pages/GalleryPage.vue'
+import { getCurrentUser } from 'aws-amplify/auth'
 
 const routes = [
   {
     path: '/',
     redirect: '/gallery',
+  },
+  {
+    path: '/login',
+    component: LoginPage,
+    name: 'login',
   },
   {
     path: '/gallery',
@@ -17,6 +24,7 @@ const routes = [
     path: '/upload',
     component: UploadPage,
     name: 'upload',
+    meta: { requiresAuth: true },
   },
   {
     path: '/viewer/:imageId',
@@ -28,6 +36,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    try {
+      await getCurrentUser()
+      next()
+    } catch (err) {
+      next('/login')
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
