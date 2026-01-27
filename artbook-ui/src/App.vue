f267a7769631e5a783a77d02e1ab5ae76e6836c4
Chris Crossman <christopher.j.crossman@gmail.com>
2026-01-26 18:49:52 -0700

    [render preview] Try this

--- artbook-ui/src/App.vue
+++ artbook-ui/src/App.vue
@@ -1,14 +1,11 @@
 <script setup>
 // This string is replaced literally during the build process
-const apiServiceUrl = import.meta.env.VITE_SERVICE_URL;
+const apiServiceUrl = import.meta.env.VITE_SERVICE_URL
+console.log("apiServiceUrl: " + apiServiceUrl)
 
-const healthMessage = await fetch(`${apiServiceUrl}/api/v1/status/health`)
-  .then((res) => res.json())
-  .then((data) => data.message)
-  .catch(() => 'Service Unavailable')
-  .finally(() => {
-    console.log('Health Message:', healthMessage)
-  })
+const apiUrl = `${apiServiceUrl}/api/v1/status/health`
+console.log("apiUrl: " + apiUrl)
+
 </script>
 
 <template>
