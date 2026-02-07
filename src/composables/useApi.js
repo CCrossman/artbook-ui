6f0384a665b7ddbcfb506e967e140ce6fbf3a7e1
Chris Crossman <christopher.j.crossman@gmail.com>
2026-02-07 13:24:35 -0700

    Fix the fetchImages usage

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -1,21 +1,29 @@
 export function useApi() {
   const baseUrl = import.meta.env.VITE_SERVICE_URL || ''
-  const daoBaseUrl = import.meta.env.VITE_DAO_HOST_URL || ''
+  const daoBaseUrl = import.meta.env.VITE_DAO_HOST || ''
 
   const fetchApi = async (endpoint, options = {}) => {
     const url = `${baseUrl}${endpoint}`
-    console.log("fetchApi with: ", url, options);
+    // console.log("fetchApi with: ", url, options);
     return fetch(url, options)
   }
 
+  const fetchImages = async (params) => {
+    const url = `${daoBaseUrl}/api/v1/images?${params.toString()}`
+    // console.log("fetchImages with: ", url);
+    return fetch(url)
+  }
+
+
   const fetchImage = async (imageId, imageType) => {
     const url = `${daoBaseUrl}/api/v1/images/${imageId}/${imageType}`
-    console.log("fetchImage with: ", url, options);
+    // console.log("fetchImage with: ", url, options);
     return fetch(url)
   }
 
   return {
     fetchApi,
     fetchImage,
+    fetchImages,
   }
 }
